acfa3d5f34d50748994e869386da59a161566f14
Wen-Binghui <wbhwenbinghui@gmail.com>
2022-08-29 19:05:13 +0200

    1st

---
+++ cmake-build-debug/CMakeFiles/Cpp_Essential.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Essential.dir/main.cpp.obj"
+  "Cpp_Essential.exe"
+  "Cpp_Essential.exe.manifest"
+  "Cpp_Essential.pdb"
+  "libCpp_Essential.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Essential.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
